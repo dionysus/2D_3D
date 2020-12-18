c145371b75a76b6650f29651a7f1b0b6bc63de6c
rkash <raag.kashyap@mail.utoronto.ca>
2020-12-18 18:54:37 -0500

    Comments for projection file

--- projection.py
+++ projection.py
@@ -1,17 +1,30 @@
 import numpy as np
-import cv2
+
 
 def norm(vector):
+    '''
+    Returns the norm of a vector
+    '''
     square = np.square(vector)
     sum_vect = np.sum(square)
     return np.sqrt(sum_vect)
 
+
 def distance_vector(camera, image_plane):
+    '''
+    Returns the corresponding unit distance vector for the camera coordinates
+    and the the point on the image plane
+    '''
     distance = image_plane-camera
     value = norm(distance)
     return distance / value
 
+
 def cross_product(distance1, distance2):
+    '''
+    Calculate the cross product of the distance vectors in order to find
+    a vector that is perpendicular to both of them
+    '''
     c1 = distance1[1]*distance2[2]-(distance2[1]*distance1[2])
     c2 = distance1[0]*distance2[2]-(distance2[0]*distance1[2])
     c3 = distance1[0]*distance2[1]-(distance2[0]*distance1[1])
@@ -19,15 +32,19 @@ def cross_product(distance1, distance2):
     value = norm(normal)
     return normal / value
 
+
 def projected_point(distance1, point1, distance2, point2):
+    '''
+    Find the coordinates of the point on the image plane projected
+    back onto the 3D space
+    '''
     # Find the unit normal vector to both distance vectors
     normal = cross_product(distance1, distance2)
-    # matrix = np.zeros((3, 3))
+    matrix = np.zeros((3, 3))
     # Create a matrix of the two distance vectors and their normal
-    # matrix[0] = distance1.T
-    # matrix[1] = -distance2.T
-    # matrix[2] = normal.T
-    matrix = np.array([distance1, -distance2, normal])
+    matrix[0] = distance1
+    matrix[1] = -distance2
+    matrix[2] = normal
     matrix = matrix.T
     # Solve the linear system of equations to find closest points
     parameters = np.linalg.solve(matrix, point2-point1)
@@ -35,11 +52,8 @@ def projected_point(distance1, point1, distance2, point2):
     first_projected = point1 + parameters[0] * distance1
     second_projected = point2 + parameters[1] * distance2
     # Find their midpoint in order to get our projected point
-    mid_x = (first_projected[0] + second_projected[0])/2
+    mid_x = (first_projected[0] + second_projected[0]) / 2
     mid_y = (first_projected[1] + second_projected[1]) / 2
     mid_z = (first_projected[2] + second_projected[2]) / 2
-    # mid_x = (first_projected[0])
-    # mid_y = (first_projected[1])
-    # mid_z = (first_projected[2])
     projected = np.array([mid_x, mid_y, mid_z])
     return projected
